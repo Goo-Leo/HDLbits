9c66087a939d5a15728c72d56c04e003603bbbbc
Goo-Leo <1063376141@qq.com>
2025-10-15 22:39:00 +0800

    多路位操作

--- Features/reduction.v
+++ Features/reduction.v
@@ -6,3 +6,16 @@ module top_module (
   assign parity = ^ in[7:0];
 
 endmodule
+
+
+module top_module(
+    input [99:0] in,
+    output out_and,
+    output out_or,
+    output out_xor
+  );
+  assign out_and = &in[99:0];
+  assign out_or = |in[99:0];
+  assign out_xor = ^in[99:0];
+
+endmodule
